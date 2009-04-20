2fe7c1b6729066aa1db9325b2b0ab5694aca9f7d
Morgan Showman <email@mshowman.com>
2009-04-20 12:00:58 -0400

    updated procedure calls to moving the laser; added basic logic, currently does not compile;

--- blackbox.adb
+++ blackbox.adb
@@ -15,32 +15,61 @@ WITH Ada.Text_IO;
 PROCEDURE BlackBox IS
 
 	-- Display Menu - Procedure (Selection : OUT Difficulty) {BlackBox}
+	Procedure DisplayMenu (Selection : OUT Difficulty) IS
+	BEGIN	-- Display Menu
+		NULL;
+	END DisplayMenu;
 	-- User Input - Procedure (Selection : OUT Option) {BlackBox}-- User Input
+	PROCEDURE UserInput (Selection : OUT Option) IS
+		Action : Boolean := False;
+	BEGIN	-- User Input
+		WHILE Action /= True LOOP
+			Get_Immediate(Selection,Action);
+		END LOOP;
+	END UserInput;
 	-- Execute - Procedure (Selection : IN Option) {BlackBox}
+	PROCEDURE Execute (Selection : IN Option) IS
+	BEGIN	-- Execute
+		Case Selection
+			-- Move - Procedure (Selection : IN Option;GameBoard : IN OUT BoardType) {Actions}
+			When Direction
+				Move(Selection,GameBoard);
+			-- Fire Laser - Procedure (GameBoard : IN OUT BoardType) {Actions}
+			When Spacebar
+				FireLaser(GameBoard);
+			-- Retract - Procedure (GameBoard : IN OUT BoardType) {Actions}
+			When R
+				Retract(GameBoard);
+			-- Guess - Procedure (GameBoard : IN OUT BoardType) {Actions}
+			When G
+				Guess(GameBoard);
+			-- Help - Procedure (GameBoard : IN OUT BoardType) {Actions}
+			When H
+				Guess(GameBoard);
+	END Execute;
 
 BEGIN
-	-- Display Menu - Procedure (Selection : OUT Difficulty) {BlackBox}
-	-- If Selection /= NULL
-		-- Setup Board (Use List/Record [BoardType] to track Windows) - Procedure (Selection : IN Difficulty; GameBoard : OUT BoardType) {Board}
-			-- Define Window
-			-- Set Border
-			-- Define Windows for Boxes
-			-- Set Colors
-			-- Define Shots Window
-			-- Define Guesses Window
-			-- Define Options Window
+	LOOP
+		-- Display Menu - Procedure (Selection : OUT Difficulty) {BlackBox}
+		DisplayMenu(Selection);
+		EXIT WHEN Selection = NULL;
+			-- Setup Board (Use List/Record [BoardType] to track Windows) - Procedure (Selection : IN Difficulty; GameBoard : OUT BoardType) {Board}
+				-- Define Window
+				-- Set Border
+				-- Define Windows for Boxes
+				-- Set Colors
+				-- Define Shots Window
+				-- Define Guesses Window
+				-- Define Options Window
 		-- Game Loop
+		WHILE NOT End_Of_Game LOOP
 			-- Update Board - Procedure (GameBoard : IN BoardType) {Board}
+			-- UpdateBoard(GameBoard);
 			-- User Input - Procedure (Selection : OUT Option) {BlackBox}
-				-- Loop While Action /= True
-					-- Get Input
+			UserInput(Selection);
 			-- Execute - Procedure (Selection : IN Option) {BlackBox}
-				-- Case Selection
-					-- Move - Procedure (GameBoard : IN OUT BoardType) {Actions}
-					-- Fire Laser - Procedure (GameBoard : IN OUT BoardType) {Actions}
-					-- Retract - Procedure (GameBoard : IN OUT BoardType) {Actions}
-					-- Guess - Procedure (GameBoard : IN OUT BoardType) {Actions}
-					-- Help - Procedure (GameBoard : IN OUT BoardType) {Actions}
-			-- Loop Until End Game Condition
-	-- Exit When Selection = Null
+			Execute(Selection);
+		-- Loop Until End Game Condition
+		END LOOP;
+	END LOOP;
 END BlackBox;
