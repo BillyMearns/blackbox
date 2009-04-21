5153fd953172f41cb5e4dc10d1f49b52119a6e0f
MorganShowman <email@mshowman.com>
2009-04-20 23:14:46 -0400

    added types

--- types.adb
+++ types.adb
@@ -1,3 +1,26 @@
-PACKAGE BODY Types IS
+PACKAGE Types IS
 
+	TYPE Boxes IS
+	RECORD
+		Mirror : Boolean := False;
+		Angle  : Boolean;
+		Window : WindowType;
+	END RECORD;
+
+	TYPE CPosition IS
+	RECORD
+		Side  : 
+		Index : Integer(1..10);
+	END RECORD;
+
+	TYPE BoardType IS
+	RECORD
+		Length			: Integer;
+		Box				: ARRAY('A'..'J',1..10) OF Boxes;
+		CarrotPosition	: CPosition;
+		Shots			: Integer;
+		Guesses			: Integer;
+		Moves			: MoveQueue;
+	END RECORD;
+	
 END Types;
